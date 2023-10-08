0159a387a9a531553ac0dfedcf9c66714ccb90d7
Adrian Rivera <riveradrian@adrianr.local>
2023-10-08 19:12:08 -0400

    feat: add mock data

---
+++ app/mocks/initialData.ts
@@ -0,0 +1,109 @@
+export type Product = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+};
+
+export const mockProducts: Product[] = [
+  {
+    id: "gid://shopify/Product/7982853619734",
+    title: "Slides",
+    description:
+      "Simple, minimal and comfortable, these slides feature a classic design in the perfect shade of iron. Whether you're just lounging around the house or running errands, these slides will offer all-day comfort.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/slides.jpg?v=1675447358",
+    price: 25.0,
+  },
+  {
+    id: "gid://shopify/Product/7982856273942",
+    title: "Sweatpants",
+    description:
+      "Soft and comfortable sweatpants in stylish shades. They are perfect for lounging with their cozy stretch fabric that offers just the right price o warmth. Enjoy the ultimate relaxation experience!",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
+    price: 35.0,
+  },
+  {
+    id: "gid://shopify/Product/7982902771734",
+    title: "Men's T-shirt",
+    description:
+      "Crafted from organic cotton, this classic T-shirt features a relaxed fit, crew neckline and timeless look. Enjoy the breathable comfort of 100% organic cotton.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410",
+    price: 40.0,
+  },
+  {
+    id: "gid://shopify/Product/7982904639510",
+    title: "Hoodie",
+    description:
+      "This hoodie is the perfect choice for comfort and warmth. Meticulously crafted from 100% cotton, the hoodie features a soft, plush fleece interior and a unisex sizing design. Soft and lightweight, it's sure to be your go-to for chilly days.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenHoodie01.jpg?v=1675455175",
+    price: 90.0,
+  },
+  {
+    id: "gid://shopify/Product/7983593357334",
+    title: "Frontpack",
+    description:
+      "This frontpack is the perfect combination of form and function, with a modern, sporty design and patented technology that enables you to easily carry numerous items on the go. It's light, comfortable and has adjustable straps to fit all body types. Plus, its water-resistant outer shell ensures your items stay dry and secure.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenFrontpack.jpg?v=1675455064",
+    price: 200.0,
+  },
+  {
+    id: "gid://shopify/Product/7983593390102",
+    title: "Half Zip",
+    description:
+      "This half zip athletic sweatshirt is designed for optimum comfort and convenience. The half zip provides easy access to slip on and off, and the lightweight fabric is breathable and flexible, ideal for active pursuits. Perfect for athletes of all levels.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenHalfzip01.jpg?v=1675455104",
+    price: 100.0,
+  },
+  {
+    id: "gid://shopify/Product/7983593422870",
+    title: "Leggings",
+    description:
+      "These sporty and lightweight leggings are designed for comfort and ease of movement. Its moisture-wicking fabric and strong seams keep you feeling cool and secure. Available in an array of colors, these leggings are an ideal choice to look stylish while exercising.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenLeggings03.jpg?v=1675455256",
+    price: 20.0,
+  },
+  {
+    id: "gid://shopify/Product/7983593947158",
+    title: "Light Puffer",
+    description:
+      "This light puffer vest is made from durable nylon and will keep you dry and comfortable in all weather. Its light coating provides reliable protection from wind and rain, and its versatile fit offers maximum mobility. Perfect for moderate temperatures and varied conditions.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/green.jpg?v=1675459832",
+    price: 80.0,
+  },
+  {
+    id: "gid://shopify/Product/7983595290646",
+    title: "Black Sunnies",
+    description:
+      "These modern black sunglasses provide 100% UV400 protection from harmful sunrays and feature mirrored lenses for a timeless and stylish look. With lightweight construction and comfortable fit, you can look cool and stay safe in any situation.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/Blacksunnies.jpg?v=1675447388",
+    price: 50.0,
+  },
+  {
+    id: "gid://shopify/Product/7983595356182",
+    title: "Gray Leather Sneakers",
+    description:
+      "These gray leather sneakers combine comfort and style for the perfect professional look. The breathable leather material ensures breathability and provides a comfortable fit, perfect for the office and other formal occasions. The handmade design is stylish and guaranteed to last.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/Greyleathersneakers.jpg?v=1675447462",
+    price: 1000.0,
+  },
+  {
+    id: "gid://shopify/Product/7983595388950",
+    title: "Gray Runners",
+    description:
+      "These gray runners are the perfect choice for running enthusiasts. These shoes provide superior breathability and comfort, so you can run longer with less fatigue. The lightweight design and airy mesh material make these shoes durable and lightweight, giving you the support you need for peak performance.",
+    image:
+      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/Greyrunners.jpg?v=1675447483",
+    price: 30.0,
+  },
+];
